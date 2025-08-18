8a8f711ff86bff8164a22882df9bd2d2c45e8424
Eduardo-Anjos <eduardoanjossantos1@gmail.com>
2025-08-18 05:08:39 -0300

    feat :sparkles:Took some classes on my project

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
+
 import {
   ConstdownContainer,
   FormContainer,
@@ -10,25 +12,34 @@ import {
 } from "./styles";
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm();
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskIntput
-            list="task-suggestions"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
-            <option value="Projeto 1"></option>
-            <option value=" Projeto 2"></option>
-            <option value="Projeto 3"></option>
-            <option value="banana"></option>
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
           </datalist>
 
-          <label htmlFor="">durante</label>
+          <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
@@ -36,22 +47,21 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
         </FormContainer>
 
         <ConstdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>00</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>00</span>
         </ConstdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
-          começar
+          Começar
         </StartCountdownButton>
       </form>
     </HomeContainer>
